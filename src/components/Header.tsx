064c386db3d74b3e7def0c92ec9ad62623b9b12d
ajay <ajayjarwal44@gmail.com>
2022-10-27 09:35:42 +0530

    header layout styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,37 +12,46 @@ const Header = () => {
   const [query, setQuery] = useState();
   const { runSearch } = useContext<PropType>(PhotoContext);
 
-  const changeHandler = (e:any) => {
+  const changeHandler = (e: any) => {
     setQuery(e.target.value);
   };
 
-  const handleSubmit = (e:any) =>{
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     if (query) {
-        runSearch(query);
+      runSearch(query);
     }
-  }
+  };
 
   return (
-    <div className="container border w-50 my-4">
-      <form className="row g-2 p-5 justify-content-center" onSubmit={handleSubmit}>
-        <div className="col-auto bg-secondary">
+    <div className="container border pt-4 w-50 my-5 bg-dark rounded shadow">
+      <form
+        className="header row g-2 justify-content-center "
+        onSubmit={handleSubmit}
+      >
+        <div className="col-sm-12 col-md-8">
           <input
             type="text"
             onChange={(e) => changeHandler(e)}
             value={query}
-            className="form-control"
+            className="form-control d-inline-block"
             placeholder="Search pohotos"
           />
         </div>
         <div className="col-auto">
-          <button className="btn btn-outline-primary">Search</button>
+          <button className="btn  btn-outline-primary">Search</button>
         </div>
       </form>
-      <div className="row mb-2  justify-content-center">
-        <Link to="river" className="btn   btn-outline-primary  col-auto">River</Link>
-        <Link to="landscape" className="btn  btn-outline-primary mx-2 col-auto">Landscape</Link>
-        <Link to="bird" className="btn btn-outline-primary  col-auto">Bird</Link>
+      <div className="header__links row mb-2 g-2 mt-5 justify-content-center">
+        <Link to="river" className="btn   btn-outline-primary  col-sm-auto ">
+          River
+        </Link>
+        <Link to="landscape" className="btn  btn-outline-primary mx-2 col-sm-auto">
+          Landscape
+        </Link>
+        <Link to="bird" className="btn btn-outline-primary  col-sm-auto">
+          Bird
+        </Link>
       </div>
     </div>
   );
