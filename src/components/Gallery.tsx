2c53cb6075afb120ef3a18a9d474db36a3599a2c
ajay <ajayjarwal44@gmail.com>
2022-10-27 10:23:30 +0530

    code refactored

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -11,13 +11,14 @@ type PropType = {
 const Gallery = () => {
     const {images} = useContext<PropType>(PhotoContext);
 
-    return ( <div className="container border border-primary">
-        <div className="row g-2 mb-5">
-            {
-                images?.map( (img:any ) => <Item key={img?.id} data={img} />)
-            }    
-        </div>
-        
+    return ( <div className="container">
+        {
+             images?.length == 0 ?
+             <h1>No image found, try another words</h1> :
+             (<div className="row g-2 mb-5">
+                { images?.map( (img:any ) => <Item key={img?.id} data={img} />)}    
+            </div>)
+        }
     </div>);
 }
  
