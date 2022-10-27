eab33961c5a657d08d022b5cc9c1827a34f2fdb0
ajay <ajayjarwal44@gmail.com>
2022-10-27 17:02:21 +0530

    rout url changed

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <PhotoContextProvider >
         <Routes >
-            <Route path='/snap-search/mountain' element={ <Container/> } />
-            <Route path='/search-snap' element={ <Container />}>
+            <Route path='/snapSearch/mountain' element={ <Container/> } />
+            <Route path='/snapSearch' element={ <Container />}>
                 <Route path=':searchQuery' element={ <Container/> } />
             </Route>
-            <Route path='/' element={ <Navigate to="/search-snap/mountain" />} />
+            <Route path='/' element={ <Navigate to="/snapSearch/mountain" />} />
         </Routes>
     </PhotoContextProvider>
   );
